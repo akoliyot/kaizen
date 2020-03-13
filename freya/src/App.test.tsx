084edca5c7a293f287e54c08c130d28a86265f8c
Aswin Koliyot <akoliyot@gmail.com>
2020-03-13 08:46:09 +0100

    chore: add linting and stuff

--- freya/src/App.test.tsx
+++ freya/src/App.test.tsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
+import { sum } from "./helpers";
 
-describe('Main tests', () => {
-  it('should render the learn react link', () => {
+describe("Main tests", () => {
+  it("should render the learn react link", () => {
     const { getByText } = render(<App />);
     const linkElement = getByText(/learn react/i);
     expect(linkElement).toBeInTheDocument();
   });
 });
 
-const sum = (a: number, b: number) => a + b;
-
-describe('Secondary', () => {
+describe("Secondary", () => {
+  // https://jestjs.io/docs/en/api#2--testeachtablename-fn-timeout-
+  // Q
+  // Using a loop test the sum function from helpers.ts
   it.each`
-      a | b | expected
-      ${1}   ${1}   ${2}
-      ${5}   ${8}   ${13}
-      ${7}   ${21}  ${28}
-    `('returns $expected when $a is added to $b', ({a, b, expected}) => {
-    let result = sum(a, b);
-    expect(result).toBe(expected);
+    a    | b    | expected
+    ${1} | ${2} | ${3}
+    ${4} | ${5} | ${9}
+  `("should return $expected when $a is added to $b", ({ a, b, expected }) => {
+    expect(sum(a, b)).toBe(expected);
   });
 });
